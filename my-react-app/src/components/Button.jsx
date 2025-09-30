cf9e5cc4a9048218eb0f974016d86be5c5aa06ee
thenix05 <tsering.tuenschitsang@outlook.com>
2025-09-30 11:36:24 +0200

    Statemanagement 1: useState Grundlegende Zählfunktion

---
+++ my-react-app/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+
+export default function Button({children}){
+    return (
+    <button className={styles.button}>
+        {children}
+    </button>)
+}
